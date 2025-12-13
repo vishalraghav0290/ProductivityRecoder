f4b8c89143c9fd35d2f1c03405996cc3fb29b261
vishal Raghav <vishalraghavmac@gmail.com>
2025-12-13 06:22:02 +0530

    Add focuslab/src/App.tsx

---
+++ focuslab/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HabitTracker from "./pages/HabitTracker";
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <HabitTracker />
+    </div>
+  );
+}
+
+export default App;
